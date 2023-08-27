41d8c81855f5955201ad2af10d30369acfdf5661
Lucille81 <milakis2601@gmail.com>
2023-08-27 15:35:35 +0300

    feat: section carousel

---
+++ pulse-project/src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    speed: 1000,
+    adaptiveHeight: true,
+    initialSlide: 1,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../icons/arrow-left.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../icons/arrow-right.svg"></button>',
+  });
+});
