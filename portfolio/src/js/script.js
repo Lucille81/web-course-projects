e05d120bdc12565bd3f24405afd739913e16c984
Lucille81 <milakis2601@gmail.com>
2023-09-27 23:48:43 +0300

    feat: section skills

--- portfolio/src/js/script.js
+++ portfolio/src/js/script.js
@@ -4,3 +4,10 @@ const burger = document.querySelector('.hamburger'),
 
 burger.addEventListener('click', () => menu.classList.add('menu_active'));
 close.addEventListener('click', () => menu.classList.remove('menu_active'));
+
+const percents = document.querySelectorAll('.skills__ratings-percent'),
+  lines = document.querySelectorAll('.skills__ratings-line span');
+
+percents.forEach((el, idx) => {
+  lines[idx].style.width = el.innerText;
+});
